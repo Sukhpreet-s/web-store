d64254b00e2b6bbe2c78d2c4c947eef9dd92f1be
Legg <41968447+Sukhpreet-s@users.noreply.github.com>
2024-08-19 01:31:14 -0400

    set up data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,62 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Product.delete_all
+User.delete_all
+ActiveStorage::Attachment.all.each { |attachment| attachment.purge }
+
+merchants = User.create!(
+  [
+    {
+      email: 'merchant1@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      role: 'merchant'
+    },
+    {
+      email: 'merchant2@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      role: 'merchant'
+    }
+  ]
+)
+
+customers = User.create!(
+  [
+    {
+      email: 'customer1@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      role: 'customer'
+    },
+    {
+      email: 'customer2@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      role: 'customer'
+    }
+  ]
+)
+
+merchants.each do |merchant|
+  merchant.products.create!(
+    [
+      {
+        name: 'Product 1 by ' + merchant.email,
+        description: 'Description for product 1 by ' + merchant.email,
+        price: 19.99,
+      },
+      {
+        name: 'Product 2 by ' + merchant.email,
+        description: 'Description for product 2 by ' + merchant.email,
+        price: 29.99,
+      }
+    ]
+  )
+end
+
+merchants.each do |merchant|
+  merchant.products.each do |product|
+    product.images.attach(io: File.open(Rails.root.join('db', 'images', 'mr-cow.jpg')), filename: 'image1.jpg')
+  end
+end
+
+puts "Seed data created successfully!"
