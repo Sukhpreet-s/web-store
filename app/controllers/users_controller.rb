8e7ea846cfabb2085721a30cd760cf80c8526ed7
Legg <41968447+Sukhpreet-s@users.noreply.github.com>
2024-08-18 23:29:43 -0400

    show errors for the sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Account created successfully!"
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
